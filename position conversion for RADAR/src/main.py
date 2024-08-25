80460788e22a8c50d762c8783b2d98e7cf28c879
rowan <rowanchin@gmail.com>
2024-08-25 23:49:34 +0800

    Update main.py

--- position conversion for RADAR/src/main.py
+++ position conversion for RADAR/src/main.py
@@ -22,31 +22,30 @@ import numpy as np
 import pandas as pd
 from PositionConversion import geo_to_enu, enu_to_rae
 
-def main():
-    # Collecting user input for the reference point
-    latr = float(input("Enter the reference latitude: "))
-    lonr = float(input("Enter the reference longitude: "))
-    altr = float(input("Enter the reference altitude (in meters): "))
+# Collecting user input for the reference point
+latr = float(input("Enter the reference latitude: "))
+lonr = float(input("Enter the reference longitude: "))
+altr = float(input("Enter the reference altitude (in meters): "))
 
-    # Collecting user input for the target point
-    latt = float(input("Enter the target latitude: "))
-    lont = float(input("Enter the target longitude: "))
-    altt = float(input("Enter the target altitude (in meters): "))
+# Collecting user input for the target point
+latt = float(input("Enter the target latitude: "))
+lont = float(input("Enter the target longitude: "))
+altt = float(input("Enter the target altitude (in meters): "))
 
-    data = {
-        'Reference_Lat': [latr],
-        'Reference_Lon': [lonr],
-        'Reference_Alt': [altr],
-        'Target_Lat': [latt],
-        'Target_Lon': [lont],
-        'Target_Alt': [altt]
-    }
+data = {
+    'Reference_Lat': [latr],
+    'Reference_Lon': [lonr],
+    'Reference_Alt': [altr],
+    'Target_Lat': [latt],
+    'Target_Lon': [lont],
+    'Target_Alt': [altt]
+}
 
-    df1 = pd.DataFrame(data)
+df1 = pd.DataFrame(data)
 
-    df1["x"], df1["y"], df1["z"] = geo_to_enu(latr,lonr,altr,latt,lont,altt) 
+df1["x"], df1["y"], df1["z"] = geo_to_enu(latr,lonr,altr,latt,lont,altt) 
 
-    df1["rng"], df1["azi"], df1["ele"] = enu_to_rae(df1["x"],df1["y"],df1["z"])
-    print(f"Range = {df1['rng'][0]} m")
-    print(f"Azimuth = {df1['azi'][0]} rad")
-    print(f"Elevation = {df1['ele'][0]} rad")
+df1["rng"], df1["azi"], df1["ele"] = enu_to_rae(df1["x"],df1["y"],df1["z"])
+print(f"Range = {df1['rng'][0]} m")
+print(f"Azimuth = {df1['azi'][0]} rad")
+print(f"Elevation = {df1['ele'][0]} rad")
